605564f96ec85f4dc5468eb2cbc748d94bddc4aa
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2021-10-12 22:00:00 -0500

    add array the tasks

--- main.go
+++ main.go
@@ -2,8 +2,32 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"io/ioutil"
+
+	"github.com/gorilla/mux"
 )
 
+type task struct {
+	ID      int    `json:"ID"`
+	Name    string `json:"Name"`
+	Content string `json:"Content"`
+}
+
+type allTasks []task
+
+var tasks = allTasks{
+	{
+		ID: 1,
+		Name: "Task one",
+		Content: "Some content"
+	}
+}
+
+func indexRoute(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Welcome to my GO API")
+}
+
 func main() {
-	fmt.Printf("Hello World")
+	router := mux.NewRouter().StrictSlash(true)
 }
