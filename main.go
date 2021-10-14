536c17065f4f4af582a37c6881d7143401a49225
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2021-10-13 22:00:00 -0500

    add function create task

--- main.go
+++ main.go
@@ -28,6 +28,14 @@ func indexRoute(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Welcome to my GO API")
 }
 
+func createTask(w http.ResponseWriter, r *http.Request) {
+	var newTask task
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(2, "Insert a valid task data")
+	}
+}
+
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 }
